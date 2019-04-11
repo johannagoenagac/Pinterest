d09121552598b3f3f184f50e37d7d6fdc8cec855
johannagoenagac <johannagoenaagc@gmail.com>
2019-04-11 18:07:37 -0300

    Updates

---
+++ precache-manifest.22563ed32a326446c1742e961e5b0c52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dea190779213edba1912",
+    "url": "/Pinterest/static/js/runtime~main.61a2ba1d.js"
+  },
+  {
+    "revision": "975043e8de5322b728a7",
+    "url": "/Pinterest/static/js/main.d95edb2c.chunk.js"
+  },
+  {
+    "revision": "3781014f1153d9e52514",
+    "url": "/Pinterest/static/js/2.5770b87a.chunk.js"
+  },
+  {
+    "revision": "975043e8de5322b728a7",
+    "url": "/Pinterest/static/css/main.5427046f.chunk.css"
+  },
+  {
+    "revision": "75c1d4d6bd958c3b1e96c8f6b54d346c",
+    "url": "/Pinterest/index.html"
+  }
+];
